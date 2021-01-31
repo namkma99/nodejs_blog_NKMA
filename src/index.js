24302849db12293279f40f80fca1449d2c85551b
NamNguyen <namkma99@gmail.com>
2021-01-31 18:36:24 +0700

    remove public/css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -16,7 +16,7 @@ app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, '/resources/views'));
 // console.log('PATH:', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
